fdcecc13a09c514cf2e35ea4e06860ee6574ce31
rodrigo33 <rbarbosa.barbosa@gmail.com>
2022-10-05 08:42:29 -0300

    Estrutura inicial do projeto

---
+++ ExemploWebAPI/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExemploWebAPI.Models
+{
+    public class Cliente
+    {
+        public string Name { get; set; }
+
+        public Cliente()
+        {
+
+        }
+    }
+}
